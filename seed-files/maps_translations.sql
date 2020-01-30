3bf64f8be0151b9979ad81b0a30ffc106aa7d759
Zemill <33404722+Zemill@users.noreply.github.com>
2020-01-30 18:20:02 -0500

    fix(seed): Added map translations

---
+++ seed-files/maps_translations.sql
@@ -0,0 +1,33 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- ------------------------------------------------------
+-- Server version	5.7.25-google-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+LOCK TABLES `maps_translations` WRITE;
+/*!40000 ALTER TABLE `maps_translations` DISABLE KEYS */;
+INSERT INTO `maps_translations` VALUES ('Braxis Holdout','BraxisHoldout','20981897'),('Braxis Holdout','BraxisHoldout','Distaccamento di Braxis'),('Braxis Holdout','BraxisHoldout','Endstation Braxis'),('Braxis Holdout','BraxisHoldout','Le laboratoire de Braxis'),('Braxis Holdout','BraxisHoldout','Placówka na Braxis'),('Braxis Holdout','BraxisHoldout','Resistencia en Braxis'),('Braxis Holdout','BraxisHoldout','Resistência de Braxis'),('Braxis Holdout','BraxisHoldout','Бойня на Браксисе'),('Braxis Holdout','BraxisHoldout','布莱克西斯禁区'),('Braxis Holdout','BraxisHoldout','브락시스 항전'),('Pull Party','PullParty','Accro au crochet'),('Pull Party','PullParty','Agganci pericolosi'),('Pull Party','PullParty','Echar el gancho'),('Pull Party','PullParty','Ich glaub, es hakt'),('Pull Party','PullParty','Impreza przy Basenie'),('Pull Party','PullParty','Pociągający Zszywa'),('Pull Party','PullParty','Puxa-puxa'),('Pull Party','PullParty','Притягательные личности'),('Pull Party','PullParty','拉钩派对'),('Pull Party','PullParty','鉤鉤派對'),('Pull Party','PullParty','갈고리 파티'),('Towers of Doom','TowersOfDoom','Les tours du destin'),('Towers of Doom','TowersOfDoom','Torres da Perdição'),('Towers of Doom','TowersOfDoom','Torres de Fatalidad'),('Towers of Doom','TowersOfDoom','Torri della Rovina'),('Towers of Doom','TowersOfDoom','Türme des Unheils'),('Towers of Doom','TowersOfDoom','Wieże Zagłady'),('Towers of Doom','TowersOfDoom','Башни Рока'),('Towers of Doom','TowersOfDoom','末日塔'),('Towers of Doom','TowersOfDoom','파멸의 탑');
+/*!40000 ALTER TABLE `maps_translations` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-01-30 18:12:32
